6c67f22ece5a2a0fc2b07f9fda86553cab797372
Mathew Chanda <mvchanda@ncsu.edu>
2021-04-16 21:45:50 -0400

    added starter files

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Mathew Chanda on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
